4bc1aed14de202b54c2c68195bae4517d1a72539
killian Planchot <killian.planchot@epitech.eu>
2021-05-20 11:06:15 +0200

    [ADD] .h file for socket

--- libs/sockets/includes/sockets.h
+++ libs/sockets/includes/sockets.h
@@ -8,4 +8,13 @@
 #ifndef C_SOCKETS_H
 #define C_SOCKETS_H
 
+int socket_create(void);
+int socket_bind(int socket_fd, int port);
+int socket_listen(int socket_fd);
+int socket_connect(int client_fd, int port, char *host);
+int socket_accept(int server_fd);
+
+int server_create(int port);
+int client_create(int port, char *host);
+
 #endif //C_SOCKETS_H
