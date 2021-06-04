992b56f88f055e02553a3f622b26267c23ae743c
Corentin Mas <corentinmas@yahoo.com>
2021-06-04 18:10:45 +0200

    [Added] Fixed list reply format

--- server/src/commands/list.c
+++ server/src/commands/list.c
@@ -15,26 +15,18 @@ static enum command_return dump_messages(t_global *global, session_t *session)
 {
     (void) global;
     char *buffer = NULL;
-    char *print = strdup("204 ");
 
-    if (!print)
-        return SYSTEM_ERROR;
     for (list_t *node = session->current_thread->replies; node; node = node->next) {
         printf("%s\n", "test");
         t_channel *channel = node->data;
         char uuid[37];
         uuid_unparse(channel->uid, uuid);
-        if (print)
-            asprintf(&buffer, "%s\"%s\" \"%s\" \"%s\"\n", print, uuid,
+
+            asprintf(&buffer, "204 \"%s\" \"%s\" \"%s\"\n", uuid,
                 channel->name, channel->desc);
-        // else
-            // asprintf(&buffer, "202 \"%s\" \"%s\" \"%s\"\n", uuid, channel->name,
-                // channel->desc);
-        free(print);
-        if (!(print = strdup(buffer)))
-            return SYSTEM_ERROR;
+
+        send_message(session->socket, buffer, RESPONSE, LIST);
     }
-    send_message(session->socket, print, RESPONSE, LIST);
     return SUCCESS;
 }
 
@@ -42,68 +34,53 @@ static enum command_return dump_threads(t_global *global, session_t *session)
 {
     (void) global, (void) session;
     char *buffer = NULL;
-    char *print = strdup("203 ");
 
-    if (!print)
-        return SYSTEM_ERROR;
     for (list_t *node = session->current_channel->messages; node; node = node->next) {
         t_messages *thread = node->data;
         char t_uuid[37];
         char u_uuid[37];
         uuid_unparse(thread->uid, t_uuid);
-        asprintf(&buffer, "%s\"%s\" \"%s\" \"%ld\" \"%s\" \"%s\"\n", print, t_uuid,
-            u_uuid, thread->created_at, thread->title, thread->body);
+        asprintf(&buffer, "203 \"%s\" \"%s\" \"%ld\" \"%s\" \"%s\"\n",
+            t_uuid, u_uuid, thread->created_at, thread->title, thread->body);
 
-        free(print);
-        if (!(print = strdup(buffer)))
-            return SYSTEM_ERROR;
+        send_message(session->socket, buffer, RESPONSE, LIST);
     }
-    send_message(session->socket, print, RESPONSE, LIST);
+
     return SUCCESS;
 }
 
 static enum command_return dump_channels(t_global *global, session_t *session)
 {
-    (void) global;
     char *buffer = NULL;
-    char *print = strdup("202");
-    if (!print)
-        return SYSTEM_ERROR;
+
     for (list_t *node = session->current_team->channels; node; node = node->next) {
         t_channel *channel = node->data;
         char uuid[37];
         uuid_unparse(channel->uid, uuid);
 
-        asprintf(&buffer, "%s \"%s\" \"%s\" \"%s\"\n", print, uuid, channel->name,
+        asprintf(&buffer, "202 \"%s\" \"%s\" \"%s\"\n", uuid, channel->name,
             channel->desc);
-        free(print);
-        if (!(print = strdup(buffer)))
-            return SYSTEM_ERROR;
+        send_message(session->socket, buffer, RESPONSE, LIST);
     }
-    send_message(session->socket, print, RESPONSE, LIST);
+
     return SUCCESS;
 }
 
 static enum command_return dump_teams(t_global *global, session_t *session)
 {
     char *buffer = NULL;
-    char *print = strdup("201 ");
 
-    if (!print)
-        return SYSTEM_ERROR;
     for (list_t *node = global->teams; node; node = node->next) {
         t_teams *team = node->data;
         char uuid[37];
         uuid_unparse(team->uid, uuid);
 
-        asprintf(&buffer, "%s\"%s\" \"%s\" \"%s\"\n", print, uuid, team->name,
+        asprintf(&buffer, "201 \"%s\" \"%s\" \"%s\"\n", uuid, team->name,
             team->desc);
 
-        free(print);
-        if (!(print = strdup(buffer)))
-            return SYSTEM_ERROR;
+        send_message(session->socket, buffer, RESPONSE, LIST);
     }
-    send_message(session->socket, print, RESPONSE, LIST);
+
     return SUCCESS;
 }
 
@@ -130,6 +107,6 @@ void command_list(t_global *global, session_t *session, char **args)
         return;
     }
     return_val = call_list(global, session);
-    if (return_val == SYSTEM_ERROR)
-        send_message(session->socket, "666 \"system error\"", RESPONSE, LIST);
+//    if (return_val == SYSTEM_ERROR)
+//        send_message(session->socket, "666 \"system error\"", RESPONSE, LIST);
 }
