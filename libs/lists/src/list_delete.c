0d62f812ae291efb3065c99429502fcdd751da08
Corentin Mas <corentinmas@yahoo.com>
2021-06-05 11:40:05 +0200

    [Fixed] List node delete function

--- libs/lists/src/list_delete.c
+++ libs/lists/src/list_delete.c
@@ -11,10 +11,11 @@
 list_t *node_delete_data(list_t *list, void *data)
 {
     LIST_CHECKPTR(data) list;
-    list_t *ptr = list;
-    for (list_t *next = ptr->next; ptr; ptr = next, next =
-        ptr ? ptr->next : NULL);
-    return node_delete(list, ptr);
+
+    for (list_t *ptr = list; ptr; ptr = ptr->next)
+        if (ptr->data == data)
+            return node_delete(list, ptr);
+    return list;
 }
 
 list_t *node_delete(list_t *list, list_t *node)
@@ -22,16 +23,14 @@ list_t *node_delete(list_t *list, list_t *node)
     LIST_CHECKPTR(node) list;
     list_t *next = node->next;
     list_t *prev = node->prev;
-    dprintf(2, "node : [%p], next: [%p], prev: [%p]\n", node, next, prev);
 
-    if (prev)
-        prev->next = next;
     if (next)
         next->prev = prev;
+    if (prev)
+        prev->next = next;
+
     free(node);
-    if (node == list)
-        return next ? next : prev;
-    return list;
+    return node == list ? next : list;
 }
 
 list_t *node_delete_fn(list_t *list, CHECKFN, void *data)
@@ -40,7 +39,7 @@ list_t *node_delete_fn(list_t *list, CHECKFN, void *data)
 
     for (list_t *l = list, *next = list->next; l; l = next, next = l->next)
         if (check_fn(l->data, data))
-            list = node_delete(list, l);
+            return node_delete(list, l);
 
     return list;
 }
