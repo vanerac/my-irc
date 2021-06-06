c50914c67f71a2eb453646be017ba442336128c0
Corentin Mas <corentinmas@yahoo.com>
2021-06-06 22:17:07 +0200

    [Fix] User looks through all users instead of all sessions

--- server/src/commands/user.c
+++ server/src/commands/user.c
@@ -12,23 +12,23 @@ void command_usr(t_global *global, session_t *session, char **args)
 {
     char *buffer;
     char uuid[37];
-    session_t *user = NULL;
+    t_user *user = NULL;
     list_t *node = NULL;
 
     if (!args || !args[0]) {
         send_message(session->socket, "665 invalid args", RESPONSE, INVALID);
         return;
     }
-    node = node_find_fn(global->sessions, find_by_uuid, args[0]);
+    node = node_find_fn(global->all_user, find_by_uuid, args[0]);
     if (!node) {
         asprintf(&buffer, "401 \"%s\"", args[0]);
         send_message(session->socket, buffer, RESPONSE, USR);
         return;
     }
-    user = (session_t *) node->data;
-    uuid_unparse(((t_user *) user->user_data)->uid, uuid);
+    user = node->data;
+    uuid_unparse(user->uid, uuid);
     asprintf(&buffer, "200 \"%s\" \"%s\" \"%i\"", uuid,
-        ((t_user *) user->user_data)->username, user->logged ? 1 : 0);
+        user->username, user->logged ? 1 : 0);
     send_message(session->socket, buffer, RESPONSE, USR);
     free(buffer);
 }
