0d0dcfc0bc25fd62eb9157e046c945e734efe359
quenelle <alexandre.quenelle@epitech.eu>
2021-06-06 22:16:37 +0200

    [MODIF] splitted function in multiples files

--- server/src/file_parser/case.c
+++ server/src/file_parser/case.c
@@ -45,33 +45,3 @@ t_channel *current_channel)
     NODE_ADD(current_channel->messages, current_thread);
     return 0;
 }
-
-void case_TEAM(char **args, t_teams *current_team, t_channel **current_channel,
-t_messages **current_thread)
-{
-    list_t *ret = NULL;
-    *current_channel = NULL, *current_thread = NULL;
-
-    current_team = parse_team(args);
-    NODE_ADD(ret, current_team);
-}
-
-int case_CHANNEL(char **args, t_teams *current_team, t_channel *current_channel,
-t_messages **current_thread)
-{
-    *current_thread = NULL;
-    current_channel = parse_channel(args);
-    if (!current_team)
-        return 84;
-    NODE_ADD(current_team->channels, current_channel);
-    return 0;
-}
-
-void case_USER(char **args)
-{
-    t_user *current_user = NULL;
-    list_t *ret = NULL;
-
-    current_user = parse_user(args);
-    NODE_ADD(ret, current_user)
-}
---
+++ server/src/file_parser/case_bis.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-BDX-4-1-myteams-corentin.mas
+** File description:
+** case_bis
+*/
+
+#include "file_parser.h"
+
+void case_TEAM(char **args, t_teams *current_team, t_channel **current_channel,
+t_messages **current_thread)
+{
+    list_t *ret = NULL;
+    *current_channel = NULL, *current_thread = NULL;
+
+    current_team = parse_team(args);
+    NODE_ADD(ret, current_team);
+}
+
+int case_CHANNEL(char **args, t_teams *current_team, t_channel *current_channel,
+t_messages **current_thread)
+{
+    *current_thread = NULL;
+    current_channel = parse_channel(args);
+    if (!current_team)
+        return 84;
+    NODE_ADD(current_team->channels, current_channel);
+    return 0;
+}
+
+void case_USER(char **args)
+{
+    t_user *current_user = NULL;
+    list_t *ret = NULL;
+
+    current_user = parse_user(args);
+    NODE_ADD(ret, current_user)
+}
